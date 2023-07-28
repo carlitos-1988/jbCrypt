86dab81deb4150ffe20c28492388ac907cd17be0
Juan C Olmedo <doubleparked88@gmail.com>
2023-07-27 19:24:12 -0700

    initial commmit

---
+++ src/main/java/com/bcrypt/jbCrypt/JbCryptApplication.java
@@ -0,0 +1,13 @@
+package com.bcrypt.jbCrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbCryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbCryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcrypt/jbCrypt/JbCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcrypt.jbCrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
